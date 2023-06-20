713c8a31f53d129dad2a4575ac5317cd29d2a7d3
devatherock <devatherock@gmail.com>
2023-06-19 20:09:27 -0500

    fix: Used absolute path to index.js in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
-set -eo pipefail
 IFS=$'\n\t'
 
 # Execute the action code and output to file
-node index.js > action.log 2>&1
+node /index.js > action.log 2>&1
+exit_code=$?
 
 # Remove lines containing sensitive information from the log
 sed -i '/::debug::/d' ./action.log
@@ -11,3 +11,6 @@ sed -i '/::add-mask::/d' ./action.log
 
 # Output the log
 cat action.log
+
+# Exit using the exit code of the node command
+exit $exit_code
