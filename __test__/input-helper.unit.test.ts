254c71d64095004fddda4e7081cd818167c1b5f7
Yuta Kasai <yuta.kasai@linecorp.com>
2025-03-30 14:16:59 +0900

    NO-ISSUE Add test

---
+++ __test__/input-helper.unit.test.ts
@@ -0,0 +1,36 @@
+// import * as core from '@actions/core'
+import {getInputs} from '../src/input-helper'
+
+describe('input-helper tests', () => {
+  const ORIGINAL_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = {...ORIGINAL_ENV}
+  })
+
+  afterAll(() => {
+    process.env = ORIGINAL_ENV
+  })
+
+  test('enableUrlCompletion should be false when "false" is passed', () => {
+    process.env['INPUT_ENABLE-URL-COMPLETION'] = 'false'
+
+    const inputs = getInputs()
+    expect(inputs.enableUrlCompletion).toBe(false)
+  })
+
+  test('enableUrlCompletion should be true when "true" is passed', () => {
+    process.env['INPUT_ENABLE-URL-COMPLETION'] = 'true'
+
+    const inputs = getInputs()
+    expect(inputs.enableUrlCompletion).toBe(true)
+  })
+
+  test('enableUrlCompletion defaults to false if not set', () => {
+    delete process.env['INPUT_ENABLE-URL-COMPLETION']
+
+    const inputs = getInputs()
+    expect(inputs.enableUrlCompletion).toBe(false)
+  })
+})
